c5f2c0eec7a8816db9670be6a395cecd39aee956
LITHIUM03 <53534129+LITHIUM03@users.noreply.github.com>
2024-07-12 17:51:05 +0200

    2 create unity test files (#8)
    * unity
    
    * craeted test stubs.
    should run with
    
    ~/repos/MyGenericTestProgram/tests$ gcc -Iunity -I../lib/LinkdedList/include/ test_LinkedList_Runner.c test_LinkedList.c unity/unity.c

---
+++ tests/test_LinkedList.c
@@ -0,0 +1,45 @@
+#include "unity.h"
+#include "LinkedList.h"
+
+void setUp(){}
+void tearDown(){}
+
+void test_CreateEmptyList()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_SetPrintElemFunc()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_SetCopyElemFunc()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_SetCompareElemFunc()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_IsListInitialized()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_InsertFront()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_InsertBack()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
+
+void test_InsertAfter()
+{
+    TEST_ASSERT_NOT_NULL(setUp);
+}
